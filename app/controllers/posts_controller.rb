2a5866d1f57cd78eb7d6f23abffc2050315e4592
debo9210 <omololuadebowale1@gmail.com>
2019-04-06 16:08:57 +0200

    make app production ready

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+
+  def index
+     @content_first = "This is some sample test for our awesome Ruby blog"
+     @content_second = "This is more sample test for our awesome Ruby blog"
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
